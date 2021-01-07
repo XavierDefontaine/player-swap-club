2456bf3bfba7c4f7beece517646e78cf4063ad87
Xavier Defontaine <xdefontaine@gmail.com>
2021-01-07 14:03:32 +0000

    Added List function

---
+++ get.js
@@ -0,0 +1,20 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+     // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
+      userId: "123",
+      gameId: event.pathParameters.id,
+    },
+  };
+
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Game not found.");
+  }
+
+  return result.Item;
+});
--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -7,6 +7,7 @@ export default function handler(lambda) {
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
+      console.log(e); // Print out the full error
       body = { error: e.message };
       statusCode = 500;
     }
